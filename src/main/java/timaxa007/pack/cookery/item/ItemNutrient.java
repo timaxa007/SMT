fd3a4bb9ba5fb31d1671f1b9e7173eac670bb69c
timaxa007 <timaxa007@gmail.com>
2015-10-31 17:09:14 +0200

    Update for Dragon2488

--- src/main/java/timaxa007/pack/cookery/item/ItemNutrient.java
+++ src/main/java/timaxa007/pack/cookery/item/ItemNutrient.java
@@ -1,9 +1,14 @@
 package timaxa007.pack.cookery.item;
 
+import timaxa007.pack.cookery.PackCookery;
+
 public class ItemNutrient extends timaxa007.core.object.ItemDummy {
 
 	public ItemNutrient(String tag) {
 		super(tag);
+		setCreativeTab(PackCookery.tabCookery);
+		setMaxDamage(1000);
+		setHasSubtypes(false);
 	}
 
 }
